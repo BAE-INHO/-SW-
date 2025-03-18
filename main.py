a3481fbd1694bbe381ef9d55bc0a37765386711d
배인호 <enobae0724@gmail.com>
2025-03-18 23:08:45 +0900

    수정

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-print("Hello World")
-n=7
+print('hello world')
 
-for i in range(1, n+1, 2):
-    print(" " * ((n-1) // 2) + "*" * i)
-
-for i in range(n-2, 0, -2):
-    print(" " * ((n-i) // 2) + "*" * i)
